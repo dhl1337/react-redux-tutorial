f1330226f530478c65e01f019d978f6998a6385e
Dan Le <dhl1337@gmail.com>
2017-01-21 14:00:08 -0700

    add ManageCoursePage component and route

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -1,3 +1,33 @@
-/**
- * Created by danle on 1/21/17.
- */
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    render() {
+        return (
+            <h1>manage course</h1>
+        );
+    }
+}
+
+ManageCoursePage.propTypes = {
+    //myProp: PropTypes.string.isRequired
+};
+
+function mapStateToProp(state, ownProps) {
+    return {
+        state: state
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProp, mapDispatchToProps)(ManageCoursePage);
