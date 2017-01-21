810f73a168e0bb0c8f92b53f5b0fad3753a5f2b1
Dan Le <dhl1337@gmail.com>
2017-01-21 13:53:37 -0700

    add course page and course list components

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -1,3 +1,29 @@
-/**
- * Created by danle on 1/21/17.
- */
+import React, {PropTypes} from 'react';
+import CourseListRow from './CourseListRow';
+
+const CourseList = ({courses, deleteCourse}) => {
+    return (
+        <table className="table">
+            <thead>
+            <tr>
+                <th>&nbsp;</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Category</th>
+                <th>Length</th>
+            </tr>
+            </thead>
+            <tbody>
+            {courses.map(course =>
+                <CourseListRow key={course.id} course={course}/>
+            )}
+            </tbody>
+        </table>
+    );
+};
+
+CourseList.propTypes = {
+    courses: PropTypes.array.isRequired
+};
+
+export default CourseList;
